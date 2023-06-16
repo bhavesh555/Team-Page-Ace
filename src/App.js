4cb73348c0649fe21c8d16452605ee7f1a1d52d8
bhavesh555 <77831291+bhavesh555@users.noreply.github.com>
2023-06-16 16:31:55 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react"; 
 import Services from "./components/Services";
 import './App.css';
- 
+import {useState, useEffect} from 'react'
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const[loading, setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000)
+  },[])
+
    return (
     <div>
-     <Services />
+      {
+        loading?
+        <ClipLoader
+        color={'#90EE90'}
+        loading={loading}
+        cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      :
+      <Services />
+
+      }
+    
     </div>
    )
  
