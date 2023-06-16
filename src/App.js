cf7e3d0e23b4906b788fa5a5e9d2a01d746a0e00
bhavesh555 <77831291+bhavesh555@users.noreply.github.com>
2023-06-16 18:03:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,25 +7,58 @@ import Particles from 'react-particles-js';
 function App() {
    return (
     <div className="App">
-     <Particles
+     {<Particles
     params={{
 	    "particles": {
 	        "number": {
-	            "value": 50
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
 	        },
 	        "size": {
-	            "value": 3
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
 	        }
 	    },
 	    "interactivity": {
 	        "events": {
 	            "onhover": {
 	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
 	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
 	            }
 	        }
 	    }
-	}} />
+	}} />}
       <Services />
     </div>
    )
